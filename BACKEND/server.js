98c5568bdefcb2a812a6917ad5967026514ba56b
IT20620202 <IT20620202@my.sliit.lk>
2022-03-20 23:28:24 +0530

    Initial commit

---
+++ BACKEND/server.js
@@ -0,0 +1,8 @@
+const express= require("express");
+const mongoose= require("mongoose");
+const dotenv= require("dotenv");
+const bodyParser= require("body-parser");
+const cors= require("cors");
+const app= express();
+
+const PORT = process.env.PORT
