794fe2bd2d728d9cb595705b18ec85b5a3eeab9a
IT20620202 <IT20620202@my.sliit.lk>
2022-03-21 12:09:17 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,27 +1,31 @@
-const express= require("express");
-const mongoose= require("mongoose");
-const dotenv= require("dotenv");
-const bodyParser= require("body-parser");
-const cors= require("cors");
-const app= express();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
 require("dotenv").config();
 
-const PORT = process.env.PORT|| 8070;
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 8070;
 
-const URL=process.env.MONGODB_URL;
-mongoose.connect(URL,
-    {
-        useCreateIndex: true,
-        useNewUrlParser:true,
-        useUnifiedTopologyL:true,
-        usrFindAndModify:true
-    })
-const connection=mongoose.connection;
-connection.once("open",()=>{
-    console.log("MongoDB Connection successfull");
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Mongodb Connection success!");
 })
-app.listen(PORT,()=>{
-console.log('Server is up and running onport number:${PORT}')
+
+//routes
+
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number: ${PORT}`);
 })
