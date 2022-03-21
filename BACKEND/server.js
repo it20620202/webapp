df8342c1c27677c4e65d976d211464bb1f756b97
IT20620202 <IT20620202@my.sliit.lk>
2022-03-21 11:11:16 +0530

    wwdw
    dedw

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,5 +4,24 @@ const dotenv= require("dotenv");
 const bodyParser= require("body-parser");
 const cors= require("cors");
 const app= express();
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT|| 8070;
+app.use(cors());
+app.use(express.json());
+
+const URL=process.env.MONGODB_URL;
+mongoose.connect(URL,
+    {
+        useCreateIndex: true,
+        useNewUrlParser:true,
+        useUnifiedTopologyL:true,
+        usrFindAndModify:true
+    })
+const connection=mongoose.connection;
+connection.once("open",()=>{
+    console.log("MongoDB Connection successfull");
+})
+app.listen(PORT,()=>{
+console.log('Server is up and running onport number:${PORT}')
+})
